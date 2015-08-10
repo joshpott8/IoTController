8d666d2d8a902eece4f5220d9da004266eb0813b
unknown <josh.potterton@LAPTOP439.ps.local>
2015-08-10 17:41:39 +0100

    Has navigation drawer and can close app

--- app/src/main/java/com/example/joshpotterton/iotcontroller/MainActivity.java
+++ app/src/main/java/com/example/joshpotterton/iotcontroller/MainActivity.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 //Written By Josh Potterton - 10/08/2015
 
@@ -21,12 +29,23 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String[] mItemTitles= {"On", "Off", "Flicker", "Blink", "Flash", "Quit", "Shutdown Pi and Quit"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.navitem, mItemTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener(this));
 
         mPager = (ViewPager) findViewById(R.id.viewPager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
@@ -47,6 +66,35 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return NUM_PAGES;
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        private AppCompatActivity activity;
+
+        public DrawerItemClickListener(AppCompatActivity act){
+            activity = act;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+           if(position < 5) {
+               mPager.setCurrentItem(position);
+               mDrawerLayout.closeDrawer(mDrawerList);
+           }
+           else{
+                switch(position){
+                    case 5:
+                        Toast.makeText(getApplicationContext(), "Quit", Toast.LENGTH_SHORT).show();
+                        finish();
+                        break;
+                    case 6:
+                        Toast.makeText(getApplicationContext(), "Shutdown Pi and Quit", Toast.LENGTH_SHORT).show();
+                        //Handle shutting down pi
+                        finish();
+                        break;
+                }
+           }
         }
     }
 
